e0a34cf0034009c98bd2a8608ecfda3ed725d35a
Jeff Petkau <jpetkau@users.noreply.github.com>
2023-02-09 21:52:53 -0800

    docs and cleanup

--- src/chain.rs
+++ src/chain.rs
@@ -1,70 +1,224 @@
-/*
+/** Iterator type returned by `cycle_n()` */
+pub struct CycleN<I> {
+    // Both `orig` and `iter` are in Option so that we don't have to
+    // call into_iter() or clone() more than necessary.
+    orig: Option<I>,
+    iter: Option<I>,
+    n: usize,
+}
 
-chain:
+/** Iterator adaptor that repeats a sequence n times.
 
-- call stdlib chain? sure why not
+    use wec::cycle_n;
 
+    assert!(cycle_n([1, 2, 3], 2).eq([1, 2, 3, 1, 2, 3]));
+
+The sequence must be `IntoIter`, and the iterator must be `Clone`.
+
+This is mostly to enable the `wec![...elems, n]` syntax.
+*/
+pub fn cycle_n<I: IntoIterator>(it: I, n: usize) -> CycleN<I::IntoIter>
+where
+    I::IntoIter: Clone,
+{
+    match n {
+        0 => CycleN {
+            orig: None,
+            iter: None,
+            n,
+        },
+        1 => {
+            let it = it.into_iter();
+            CycleN {
+                orig: None,
+                iter: Some(it),
+                n,
+            }
+        }
+        _ => {
+            let i1 = it.into_iter();
+            let i2 = i1.clone();
+            CycleN {
+                orig: Some(i1),
+                iter: Some(i2),
+                n,
+            }
+        }
+    }
+}
+
+impl<I: Iterator + Clone> Iterator for CycleN<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(it) = &mut self.iter {
+            if let Some(x) = it.next() {
+                return Some(x);
+            }
+            self.n -= 1;
+            if self.n < 2 {
+                self.iter = self.orig.take();
+            } else {
+                self.iter = self.orig.clone();
+            }
+        }
+        None
+    }
+}
+
+/**
+Chain one more elements or iterables together into one sequence, using "spread"
+syntax.
+
+    # use {wec::iter, std::array};
+
+    let arr = [1, 2, 3];
+    let vec = vec![8, 9, 10];
+
+    assert!(
+        iter![...arr, 4, ...(5..7), 7, ...vec]
+        .eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+`iter![a, b, c]` simply returns an iterator over those elements, exactly like
+`[a, b, c].into_iter()`. But elements can be prefixed with `...` to indicate
+a "spread": they are chained into the resulting iterator with
+`std::iter::chain()`.
+
+See `[wec::wec]` in this crate for more explanation of the syntax.
+
+# Examples
+
+    use {wec::{iter, CycleN}, std::{array, iter}};
+    let ints = [1, 2, 3];
+
+    // Empty invocations of iter! expand to [std::iter::empty].
+    let _: iter::Empty<i32> = iter![];
+
+    // One or more arguments expand to Array::IntoIter.
+    let _: array::IntoIter<_, 1> = iter![1];
+    let _: array::IntoIter<_, 3> = iter![1, 2, 3];
+
+    // The `[x; n]` syntax expands to [std::iter::repeat(x).take(n)]
+    let _: iter::Take<iter::Repeat<_>> = iter![0; 5];
+
+    // The `[...x; n]` syntax expands to [`cycle_n`] from this crate
+    let _: CycleN<_> = iter![...ints; 3];
+
+    // And chaining expands to [std::iter::Chain]
+    let _: iter::Chain<_, _> = iter![...ints, ...ints];
+
+# See also
+
+The `wec![]` macro in this crate is a drop-in replacement for `vec![]`,
+but enables the same spread syntax. It is equivalent to
+`iter![].collect::<Vec<_>>()`.
+
+The `itertools::chain!` macro is similar, except it takes only iterable
+arguments rather than a mix of iterables and single elements.
+
+```ignore
+    chain![a, b, iter::once(c)] == iter![...a, ...b, c]
+```
 */
 #[macro_export]
-macro_rules! chain {
+macro_rules! iter {
     // empty
     () => { ::std::iter::empty() };
 
+    // [x; n]
+    ($x:tt; $n:expr) => {
+        ::std::iter::repeat($x).take($n)
+    };
+
+    // [...x; n]
+    (...$x:tt; $n:expr) => {
+        $crate::cycle_n($x, $n)
+    };
+
+    // [...[x; n] at start
+    (...[...$xs:expr; $n:expr] $($tail:tt)*) => {
+        $crate::iter![...$crate::cycle_n($xs, $n) $($tail)*]
+    };
+
+    // simple array at start
     (...$some:tt $(,)? ) => {
         ::std::iter::IntoIterator::into_iter($some)
     };
 
-    (...$some:tt, ...$spread:expr $(,)?) => {
-        ::std::iter::Iterator::chain(
-            ::std::iter::IntoIterator::into_iter($some),
-            $spread)
+    // ...[...x; n] in second item; convert to repeat_n
+    (...$some:tt, ...[...$x:tt; $n:expr] $($tail:tt)*) => {
+        $crate::iter![...$some, ...$crate::cycle_n($x, $n) $($tail)*]
     };
 
-    (...$some:tt, ...$spread:expr, $($tail:tt)*) => {
-        $crate::chain![
+    // ...[x; n] in second item; convert to simple expr
+    (...$some:tt, ...[$x:tt; $n:expr] $($tail:tt)*) => {
+        $crate::iter![...$some, ...std::iter::repeat($x).take($n) $($tail)*]
+    };
+
+    // ...xs in second item and nothing else; final chain()
+    (...$some:tt, ...$xs:expr $(,)?) => {
+        ::std::iter::Iterator::chain(
+            ::std::iter::IntoIterator::into_iter($some),
+            $xs)
+    };
+
+    // ...xs in second item followed by more; continue chain()
+    (...$some:tt, ...$xs:expr, $($tail:tt)*) => {
+        $crate::iter![
             ... ::std::iter::Iterator::chain(
                 ::std::iter::IntoIterator::into_iter($some),
-                $spread),
+                $xs),
             $($tail)*
         ]
     };
 
     // shift a single elem into the starting literal
-    (...[$($some:tt)*], $elem:tt $(, $($tail:tt)*)?) => {
-        $crate::chain![...[$($some)*, $elem], $($($tail)*)?]
+    (...[$($some:tt)*], $x:tt $(, $($tail:tt)*)?) => {
+        $crate::iter![...[$($some)*, $x], $($($tail)*)?]
     };
 
     // first element is a (non-literal) spread
-    (...$spread:expr $(, $($tail:tt)*)?) => {
+    (...$xs:expr $(, $($tail:tt)*)?) => {
         ::std::iter::Iterator::chain(
-            ::std::iter::IntoIterator::into_iter($spread),
-            $crate::chain![ $($($tail)*)? ])
+            ::std::iter::IntoIterator::into_iter($xs),
+            $crate::iter![ $($($tail)*)? ])
     };
 
     // shift a single elem into a new starting literal
-    ($elem:expr $(, $($tail:tt)*)?) => {
-        $crate::chain![...[$elem], $($($tail)*)? ]
+    ($x:expr $(, $($tail:tt)*)?) => {
+        $crate::iter![...[$x], $($($tail)*)? ]
     };
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_chain() {
-        assert_eq!(Vec::<u32>::new(), chain![].collect::<Vec<_>>());
+    fn test_cycle_n() {
+        assert!(cycle_n(1..4, 0).eq([]));
+        assert!(cycle_n(1..4, 1).eq([1, 2, 3]));
+        assert!(cycle_n(1..4, 2).eq([1, 2, 3, 1, 2, 3]));
 
-        assert_eq!(vec![1], chain![1].collect::<Vec<_>>());
-        assert_eq!(vec![1, 2], chain![1, 2].collect::<Vec<_>>());
-        assert_eq!(vec![1, 2, 3], chain![1, 2, 3].collect::<Vec<_>>());
+        assert!(cycle_n(0..0, 10).eq([]));
+    }
 
-        assert_eq!(vec![1], chain![...[1]].collect::<Vec<_>>());
-        assert_eq!(vec![1, 2], chain![...[1,2]].collect::<Vec<_>>());
+    #[test]
+    fn test_iter() {
+        assert_eq!(Vec::<u32>::new(), iter![].collect::<Vec<_>>());
 
-        assert_eq!(vec![1, 2], chain![1,...[2]].collect::<Vec<_>>());
-        assert_eq!(vec![1, 2, 3], chain![...[1], ...[2,3]].collect::<Vec<_>>());
+        assert_eq!(vec![1], iter![1].collect::<Vec<_>>());
+        assert_eq!(vec![1, 2], iter![1, 2].collect::<Vec<_>>());
+        assert_eq!(vec![1, 2, 3], iter![1, 2, 3].collect::<Vec<_>>());
+
+        assert_eq!(vec![1], iter![...[1]].collect::<Vec<_>>());
+        assert_eq!(vec![1, 2], iter![...[1,2]].collect::<Vec<_>>());
+
+        assert_eq!(vec![1, 2], iter![1,...[2]].collect::<Vec<_>>());
+        assert_eq!(vec![1, 2, 3], iter![...[1], ...[2,3]].collect::<Vec<_>>());
         assert_eq!(
             vec![1, 2, 3, 4, 5, 6],
-            chain![1, ...[2,3], 4, ...[], 5, ...[6]].collect::<Vec<_>>()
+            iter![1, ...[2,3], 4, ...[], 5, ...[6]].collect::<Vec<_>>()
         );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,100 @@
-/*
-counting items?
- - tricky with extend() etc
- - recurse twice. First time builds up size and iters, second time applies them
-
-first pass:
-  let iters = (elem.into_iter(), iters)
-  let n = n + iters.0.size_hint();
-
-  let n = n + 1;
-
-  let iters = (elem.into_iter(), iters)
-  let n = n + iters.0.size_hint();
-
-second pass:
-  v = Vec::with_capacity(n);
-  v.extend(iters.1.1.1.0);
-  v.push(something);
-  v.extend(iters.1.0);
-  v.extend(iters.0);
-  v
-
-this means you can't do:
-
-  wec![...xs.iter().cloned(), ...xs]
-
-though you can do:
-
-  wec![...xs.clone(), ...xs]
-
-is there some way to get the size hint without calling IntoIterator?
-Don't think so.
-
-Chain:
-
+/*!
+This crate defines a few macros and utilities to enable nicer literal
+syntax.
 */
 mod chain;
 
+pub use chain::{cycle_n, CycleN};
+
+/**
+A drop-in replacement for `vec![]` that adds "spread" syntax.
+
+    # use {wec::wec, std::array};
+
+    let arr = [1, 2, 3];
+    let vec = vec![8, 9, 10];
+
+    assert_eq!(
+        wec![...arr, 4, ...(5..7), 7, ...vec],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+`wec![a, b, c]` simply returns a vec of those elements, exactly like `vec!`.
+But elements can be prefixed with `...` to indicate a "spread", which is
+expanded by calling `into_iter()`.
+
+# Examples
+
+Normal `vec!` syntax works the same way:
+
+    use wec::wec;
+
+    assert_eq!(wec![1, 2, 3], vec![1, 2, 3]);
+    assert_eq!(wec!["x"; 5], vec!["x"; 5]);
+
+Spread syntax `[...xs]` inserts xs into the result via `into_iter()`:
+
+    use wec::wec;
+    let abc = [1, 2, 3];
+
+    assert_eq!(wec![...abc, ...abc], [1, 2, 3, 1, 2, 3]);
+
+You can also use `[...xs; n]` to repeat a sequence n times, like
+`std::iter::cycle()` but of finite length. The iterator returned
+by `xs.into_iter()` must implement `Clone`.
+
+    use wec::wec;
+
+    let abc = [1, 2, 3];
+    assert_eq!(
+        wec![...abc; 2], [1, 2, 3, 1, 2, 3])
+
+And all these can be combined:
+
+    use wec::wec;
+    let abc = [1, 2, 3];
+
+    // Note that (3..5) here is an ordinary range expression,
+    // not special syntax.
+    assert_eq!(
+        wec![1, ...[2;2], ...[...(3..5);2]],
+        [1, 2, 2, 3, 4, 3, 4]);
+
+    assert_eq!(
+        wec![0, ...[...abc; 2], ...[9; 3]],
+        [0, 1, 2, 3, 1, 2, 3, 9, 9, 9]);
+
+The cycle-n-times logic is also available as a function, `cycle_n`
+since it's missing from std::iterator.
+
+# Design choices
+
+Why `...` when most range-like things in Rust, including the existing
+pattern spread syntax, use ".."? Because `..x` is already an expression
+of type `std::ops::RangeTo`. The macro could still work with `..`, and
+you could disambiguiate by parenthesizing if necessary:
+
+```ignore
+    let r = [..1, ..2];
+    let my_ranges = wec![(..1), ..r];
+```
+
+But it seemed better to sidestep the issue.
+
+Why prefix instead of suffix syntax, like `wec![a, bs..., c]`? Mostly
+to match other languages I'm aware of with spread syntax.
+
+# See also
+
+The `iter![]` macro in this crate uses the same syntax, but produces
+an iterator instead of a Vec. In fact `wec::wec[...]` is  equivalent to
+`iter![...].collect::<Vec<_>>()`.
+*/
 #[macro_export]
 macro_rules! wec {
     () => { Vec::new() };
 
     ($($tail:tt)*) => {
-        ::std::iter::Iterator::collect::<Vec<_>>($crate::chain![$($tail)*])
+        ::std::iter::Iterator::collect::<Vec<_>>($crate::iter![$($tail)*])
     };
 }
 
